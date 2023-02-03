317d072cd0ce82d8541330e90fad79e4686a9a9f
janosick <arcadego337@gmail.com>
2023-02-03 17:20:12 +0100

    car

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function Car(carName){
     this.carName = carName || 'Unknown car';
 }
-car.prototype.logName = function () {
+Car.prototype.logName = function () {
     console.log('Car Name is', this.carName);
 };
 
